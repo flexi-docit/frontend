c69a27d2f4bcd36b9a6695948fd99e0f07193b3e
Ojas Inamdar <ojasinamdar101@gmail.com>
2023-02-11 15:31:51 +0530

    [feat]: Basic login module (#8)
    * [doc]: Proposed auth workflow
    
    * [feat]: Basic login module

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-
+  <div v-if="this.$store.state.loggedIn">
+    <p>Mock API call here to data</p>
+  </div>
+  <div v-else>
+    <router-link to="/login">Login</router-link>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import router from '@/router';
+import checkAndManageAuth from '@/utils/checkAuth';
+
+export default {
+  created() {
+    checkAndManageAuth(this.$store, router);
+
+    // We should have state here onwards
   }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class = 'login-container'>
-        
-        <div id = "logo-name-container">
-            <img :src="logoname" id = "logo-name-img" />
+    <div class='login-container'>
+        <div id="logo-name-container">
+            <img :src="logoname" id="logo-name-img" />
         </div>
-        <div class = "login">
-            <div class = "bg-img" :style="background"></div>
-            <img :src = "logoicon" id = "logo-icon-img"/><br/>
-            <h3 class = "login-text" id = "uname">Username<br /></h3>
-            <input class = "login-input" type = "text" /><br />
-            <h3 class = "login-text">Password<br /></h3>
-            <input class = "login-input" type = "text" /><br />
-            <h3 class = "login-text" id = "login-forgot" >Forgot Password?<br /></h3>
-            <button id = 'sign-in'>SIGN IN</button>
-        </div>
+        <form class="login" @submit.prevent="handleSubmit">
+            <div class="bg-img" :style="background"></div>
+            <img :src="logoicon" id="logo-icon-img" /><br />
+
+            <label for="email" class="login-text" id="uname">Email<br /></label>
+            <input class="login-input" id="email" type="email" v-model.trim="email" /><br />
+            <label for="password" class="login-text">Password<br /></label>
+            <input class="login-input" id="password" type="password" v-model.trim="password" /><br />
+
+            <p class="login-text" id="login-forgot">Forgot Password?<br /></p>
+
+            <button id='sign-in' type="submit">SIGN IN</button>
+        </form>
         <div>
 
         </div>
@@ -21,26 +23,86 @@
 </template>
 
 <script>
-    import logoicon from "../assets/common/logo-icon.svg";
-    import logoname from "../assets/common/logo-name.svg";
-    import bg from "../assets/login/bg.svg";
-    export default {
-        data() {
+import router from "@/router";
+import logoicon from "../assets/common/logo-icon.svg";
+import logoname from "../assets/common/logo-name.svg";
+import bg from "../assets/login/bg.svg";
+
+import { localStorageConstants } from "../utils/constants";
+
+export default {
+    data() {
         return {
             logoicon,
             logoname,
-            background: { backgroundImage: `url(${bg})` }
+            background: { backgroundImage: `url(${bg})` },
+
+            email: '',
+            password: '',
         }
-    } 
+    },
+    methods: {
+        async handleSubmit() {
+            const url = "http://localhost:8000/api/v1/auth/login";
+
+            if (!this.password || this.password.length <= 0) return alert("Password must be provided");
+            const data = { email: this.email, password: this.password };
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                const formattedResponse = await response.json();
+                if (formattedResponse.status) {
+                    if (formattedResponse.data) {
+                        localStorage.setItem('d_token', formattedResponse.data.token);
+                        localStorage.setItem('d_role', formattedResponse.data.role);
+                        localStorage.setItem('d_id', formattedResponse.data.id);
+                        this.$store.commit('setRole', formattedResponse.data.role);
+                        this.$store.commit('setID', formattedResponse.data.id);
+                        this.$store.commit('setLoggedIn', true);
+                        router.push('/')
+                    }
+                }
+                else {
+                    console.error(formattedResponse);
+                    alert("Error! Check your credentials again please");
+                }
+            } catch (error) {
+                console.error(error);
+                alert("Error! Please try again later");
+            }
+        }
+    },
+    created() {
+        const { tokenKey, roleKey, IDKey } = localStorageConstants;
+
+        const JWTRegex = /^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/;
+        const token = localStorage.getItem(tokenKey);
+        const role = localStorage.getItem(roleKey);
+        const ID = localStorage.getItem(IDKey);
+        if (JWTRegex.test(token) && role !== null && ID !== null) {
+            window.alert("Logged in, redirecting...");
+            this.$store.commit('setLoggedIn', true);
+            this.$store.commit('setRole', role);
+            this.$store.commit("setID", ID);
+            router.push("/");
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
 .login-container {
     font-family: 'Roboto';
     display: flex;
-    
+
 }
+
 .bg-img {
     background-size: cover;
     position: absolute;
@@ -49,6 +111,7 @@
     z-index: -10;
     width: 50vw;
     height: 100vh;
+
     @media screen and (max-width: 768px) {
         top: 80px;
         width: 100vw;
@@ -62,20 +125,17 @@
     align-content: center;
     left: 0;
     width: 50vw;
+
     @media screen and (max-width: 768px) {
         display: none;
     }
 }
 
-#logo-name-img {
-
-}
-
 .login {
     flex: 1;
     width: 50vw;
-    @media screen and (max-width: 768px)
-    {
+
+    @media screen and (max-width: 768px) {
         height: 100vh;
     }
 }
@@ -92,12 +152,13 @@
 
 #uname {
     margin-top: 60px;
+
     @media screen and (max-width: 768px) {
         margin-top: 30px;
     }
 }
 
-.login-text {  
+.login-text {
     color: #FFFFFF;
     font-weight: 500;
     font-size: 27px;
@@ -106,6 +167,7 @@
     margin-top: 15px;
     margin-bottom: 15px;
     margin-right: 330px;
+
     @media screen and (max-width: 768px) {
         margin-right: 150px;
         font-size: 15px;
@@ -121,6 +183,7 @@
     width: 474px;
     border-radius: 7px;
     border-width: 0px;
+
     @media screen and (max-width: 768px) {
         padding-left: 10px;
         font-size: 15px;
@@ -134,6 +197,7 @@
     margin-right: 0px;
     margin-top: 50px;
     margin-bottom: 75px;
+
     @media screen and (max-width: 768px) {
         margin-top: 20px;
         margin-bottom: 40px;
@@ -150,6 +214,7 @@
     font-size: 26px;
     border-radius: 18px;
     border-width: 0px;
+
     @media screen and (max-width: 768px) {
         width: 142px;
         height: 33px;
@@ -157,5 +222,4 @@
         font-size: 16px;
     }
 }
-
 </style>
