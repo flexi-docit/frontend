97c1cfe08e3d33e7e74a8c2b3cf4fcf43de1ab30
Achyut <achyut.shukla.btech2020@sitpune.edu.in>
2023-02-22 16:00:17 +0530

    lock files included in ignore

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,25 @@
 <template>
-    <main class='login-container'>
-        <figure class="login-container-left">
-            <img class="login-bg-img" :src="background" alt="" />
-        </figure>
-        <form class="login-form" @submit.prevent="handleSubmit">
-            <img :src="logo" class="logo-icon-img" /><br />
-            <label for="email" class="login-label">Email</label>
-            <input class="login-input" id="email" type="email" v-model.trim="email" />
-            <label for="password" class="login-label">Password</label>
-            <input class="login-input" id="password" type="password" v-model.trim="password" required />
+  <main class="login-container">
+    <figure class="login-container-left">
+      <img class="login-bg-img" :src="background" alt="" />
+    </figure>
+    <form class="login-form" @submit.prevent="handleSubmit">
+      <img :src="logo" class="logo-icon-img" /><br />
+      <label for="email" class="login-label">Email</label>
+      <input class="login-input" id="email" type="email" v-model.trim="email" />
+      <label for="password" class="login-label">Password</label>
+      <input
+        class="login-input"
+        id="password"
+        type="password"
+        v-model.trim="password"
+        required
+      />
 
-            <p>Forgot Password?<br /></p>
-            <button type="submit">SIGN IN</button>
-        </form>
-    </main>
+      <p>Forgot Password?<br /></p>
+      <button type="submit">SIGN IN</button>
+    </form>
+  </main>
 </template>
 
 <script>
@@ -24,140 +30,138 @@ import background from "../assets/login/bg.svg";
 import { localStorageConstants } from "../utils/constants";
 
 export default {
-    data() {
-        return {
-            logo,
-            background,
+  data() {
+    return {
+      logo,
+      background,
 
-            email: '',
-            password: '',
-        }
-    },
-    methods: {
-        async handleSubmit() {
-            const url = "http://localhost:8000/api/v1/auth/login";
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      const url = "http://localhost:8000/api/v1/auth/login";
 
-            if (!this.password || this.password.length <= 0) return alert("Password must be provided");
-            const data = { email: this.email, password: this.password };
-            try {
-                const response = await fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
+      if (!this.password || this.password.length <= 0)
+        return alert("Password must be provided");
+      const data = { email: this.email, password: this.password };
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
 
-                const formattedResponse = await response.json();
-                if (formattedResponse.status) {
-                    if (formattedResponse.data) {
-                        localStorage.setItem('d_token', formattedResponse.data.token);
-                        localStorage.setItem('d_role', formattedResponse.data.role);
-                        localStorage.setItem('d_id', formattedResponse.data.id);
-                        this.$store.commit('setRole', formattedResponse.data.role);
-                        this.$store.commit('setID', formattedResponse.data.id);
-                        this.$store.commit('setLoggedIn', true);
-                        router.push('/')
-                    }
-                }
-                else {
-                    console.error(formattedResponse);
-                    alert("Error! Check your credentials again please");
-                }
-            } catch (error) {
-                console.error(error);
-                alert("Error! Please try again later");
-            }
-        }
-    },
-    created() {
-        const { tokenKey, roleKey, IDKey } = localStorageConstants;
-
-        const JWTRegex = /^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/;
-        const token = localStorage.getItem(tokenKey);
-        const role = localStorage.getItem(roleKey);
-        const ID = localStorage.getItem(IDKey);
-        if (JWTRegex.test(token) && role !== null && ID !== null) {
-            window.alert("Logged in, redirecting...");
-            this.$store.commit('setLoggedIn', true);
-            this.$store.commit('setRole', role);
-            this.$store.commit("setID", ID);
+        const formattedResponse = await response.json();
+        if (formattedResponse.status) {
+          if (formattedResponse.data) {
+            localStorage.setItem("d_token", formattedResponse.data.token);
+            localStorage.setItem("d_role", formattedResponse.data.role);
+            localStorage.setItem("d_id", formattedResponse.data.id);
+            this.$store.commit("setRole", formattedResponse.data.role);
+            this.$store.commit("setID", formattedResponse.data.id);
+            this.$store.commit("setLoggedIn", true);
             router.push("/");
+          }
+        } else {
+          console.error(formattedResponse);
+          alert("Error! Check your credentials again please");
         }
+      } catch (error) {
+        console.error(error);
+        alert("Error! Please try again later");
+      }
+    },
+  },
+  created() {
+    const { tokenKey, roleKey, IDKey } = localStorageConstants;
+
+    const JWTRegex = /^[A-Za-z0-9-_=]+.[A-Za-z0-9-_=]+.?[A-Za-z0-9-_.+/=]*$/;
+    const token = localStorage.getItem(tokenKey);
+    const role = localStorage.getItem(roleKey);
+    const ID = localStorage.getItem(IDKey);
+    if (JWTRegex.test(token) && role !== null && ID !== null) {
+      window.alert("Logged in, redirecting...");
+      this.$store.commit("setLoggedIn", true);
+      this.$store.commit("setRole", role);
+      this.$store.commit("setID", ID);
+      router.push("/");
     }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .login-container {
-    font-family: 'Roboto';
+  font-family: "Roboto";
+  display: flex;
+  width: 100vw;
+  height: calc(100vh - 90px);
+  overflow: hidden;
+
+  @media screen and (max-width: 768px) {
+    height: calc(100vh - 85px);
+  }
+
+  > .login-form {
     display: flex;
-    width: 100vw;
-    height: calc(100vh - 90px);
-    overflow: hidden;
+    flex-direction: column;
+    row-gap: 10px;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    padding: 15px 100px 15px 100px;
+    height: 100%;
 
     @media screen and (max-width: 768px) {
-        height: calc(100vh - 85px);
+      width: 100%;
     }
 
-
-    >.login-form {
-        display: flex;
-        flex-direction: column;
-        row-gap: 10px;
-        align-items: center;
-        justify-content: center;
-        width: 50%;
-        padding: 15px 100px 15px 100px;
-        height: 100%;
-
-        @media screen and (max-width: 768px) {
-            width: 100%;
-        }
-
-        >.logo-icon-img {
-            height: 120px;
-        }
-
-
-        >.login-label {
-            font-weight: 500;
-            font-size: 22px;
-            text-align: left;
-            width: 100%;
-            margin-left: 20px;
-        }
-
-        >button {
-            background-color: #DB1A32;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            width: 60%;
-            min-height: 40px;
-            margin-top: 40px;
-        }
-
-        >input {
-            background-color: #EEEEF6;
-            border: none;
-            padding: 10px;
-            width: 100%;
-        }
+    > .logo-icon-img {
+      height: 120px;
     }
 
-    >.login-container-left {
-        width: 50%;
-
-        @media screen and (max-width: 768px) {
-            display: none;
-        }
-
-        >img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
+    > .login-label {
+      font-weight: 500;
+      font-size: 22px;
+      text-align: left;
+      width: 100%;
+      margin-left: 20px;
     }
+
+    > button {
+      background-color: #db1a32;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      width: 60%;
+      min-height: 40px;
+      margin-top: 40px;
+    }
+
+    > input {
+      background-color: #eeeef6;
+      border: none;
+      padding: 10px;
+      width: 100%;
+    }
+  }
+
+  > .login-container-left {
+    width: 50%;
+
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
+
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 }
 </style>
